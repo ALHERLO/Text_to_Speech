0dd3d212a51848e8ce72f36e516e5d7c32d0f90e
Alan Hernandez <101542139+ALHERLO@users.noreply.github.com>
2022-10-30 21:58:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ path=""
 file_name=""
 playing=False
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/alanhernandez/Downloads/swift-firmament-365303-039e5e7f9d95.json"
+
 
 #Find path of PDF file with filedialog
 def filename_select(event) -> str:
@@ -81,49 +81,7 @@ def text_to_speech_gcs(event) -> None:
         out.write(response.audio_content)
         print(f'Audio content written to file {file_name}')
 
-# # creating a pdf file object
-# pdfFileObj = open('/Users/alanhernandez/PycharmProjects/Text To Speech Script/beauty_and_the_beast.pdf', 'rb')
-#
-# # creating a pdf reader object
-# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-#
-# # printing number of pages in pdf file
-# print(pdfReader.numPages)
-#
-# # creating a page object
-# pageObj = pdfReader.getPage(1)
-#
-# # extracting text from page
-# print(type(pageObj.extractText()))
-#
-# text_extraction(pageObj.extractText())
-#
-# # closing the pdf file object
-# pdfFileObj.close()
-
-
-
-# # Instantiates a client
-# client = texttospeech.TextToSpeechClient()
-# # Set the text input to be synthesized
-# synthesis_input = texttospeech.SynthesisInput(text=text_extraction(path))
-# # Build the voice request, select the language code ("en-US") and the ssml
-# # voice gender ("neutral")
-# voice = texttospeech.VoiceSelectionParams(language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL)
-# # Select the type of audio file you want returned
-# audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
-# # Perform the text-to-speech request on the text input with the selected
-# # voice parameters and audio file type
-# response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
-# # The response's audio_content is binary.
-# with open("output.mp3", "wb") as out:
-#     # Write the response to the output file.
-#     out.write(response.audio_content)
-#     print('Audio content written to file "output.mp3"')
-
 BACKGROUND_COLOR = '#E9E9E9'
-
-print(path)
 
 window = Tk()
 window.title("Audiobook Maker")
